107b48dd01eea8c401bdbab54f9a91cb2ec5cd83
ydauvin <ydauvin@enssat.fr>
2020-03-11 17:25:33 +0100

    1er

---
+++ test.py
@@ -0,0 +1,9 @@
+#-*- coding: utf-8 -*-
+import math
+from matplotlib.pyplot import *
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+import numpy
+
+def validecorde(i,j):
+    printf("a")

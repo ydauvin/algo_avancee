04e26b263aa0e45c412164d4fc6ddf0848944014
ydauvin <ydauvin@enssat.fr>
2020-03-13 10:09:25 +0100

    QSDFGHJK

--- algo.py
+++ algo.py
@@ -1,18 +1,29 @@
 #-*- coding: utf-8 -*-
-#import math
+import math
 #import numpy
 #from scipy import *
-n = 5
-tabcorde= []
+n = 7
+tabcorde= [(0,2),(0,3)]
 
 
 def main():
-    validecorde(1, 2)
+    print(validecorde(7, 3))
 
 
 def validecorde(i,j) :
-    l=len(tabcorde)
-    print(l)
+    l = len(tabcorde)
+    if(l>=n-3):
+        return False
+    if(i>j):
+        tmp=j
+        j=i
+        i=tmp
+    if(abs(i-j)>1):
+        for k in tabcorde :
+            if(k[0]==i and k[1]==j):
+                return False
+            elif k[0]
+    return True
 
 
 

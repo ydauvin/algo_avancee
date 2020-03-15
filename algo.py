49005041f006d0b21ba08b7d833dac272e266ed1
ydauvin <ydauvin@enssat.fr>
2020-03-16 00:37:25 +0100

    valider corde

--- algo.py
+++ algo.py
@@ -9,7 +9,6 @@ tabcorde= [(0,2),(0,3)]
 def main():
     print(validecorde(7, 3))
 
-
 def validecorde(i,j) :
     l = len(tabcorde)
     if(l>=n-3):
@@ -22,7 +21,10 @@ def validecorde(i,j) :
         for k in tabcorde :
             if(k[0]==i and k[1]==j):
                 return False
-            elif k[0]
+            elif (k[0]<i and k[1]>i) and (j<k[0] or j>k[1]):
+                return False
+            elif (k[0]<j and k[1]>j) and (i<k[0] or i>k[1]):
+                return False
     return True
 
 

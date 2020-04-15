3bf6549fd994851ba98b514ac91f30042881722e
ydauvin <51073361+ydauvin@users.noreply.github.com>
2020-04-15 15:38:55 +0200

    repartition en plusieur fichier

--- glouton.py
+++ glouton.py
@@ -1,22 +1,24 @@
-from algo import *
+from main import *
+from polygone import *
 
 
 def corde_exterieur():
-    tablong={}
-    l=len(polygone)
-    i=0
-    while(len(tablong)!=l):
-        tablong[longueur(i,(i+2)%l)]=(i,(i+2)%l)
-        i+=1
+    tablong = {}
+    l = len(polygone)
+    i = 0
+    while len(tablong) != l:
+        tablong[longueur(i, (i + 2) % l)] = (i, (i + 2) % l)
+        i += 1
     return tablong
 
+
 def glouton():
-    tablong=corde_exterieur()
-    while(len(tabcorde)!=len(polygone)):
-        if(tablong=={}):
+    tablong = corde_exterieur()
+    while len(tabcorde) != len(polygone):
+        if tablong == {}:
             break
-        i=tablong[min(tablong)][0]
-        j=tablong[min(tablong)][1]
-        if(validecorde(i,j)):
-            tabcorde.append((i,j))
+        i = tablong[min(tablong)][0]
+        j = tablong[min(tablong)][1]
+        if validecorde(i, j):
+            tabcorde.append((i, j))
         del tablong[min(tablong)]
--- polygone.py
+++ polygone.py
@@ -1,14 +1,31 @@
-n=7
-polygone=[(0,10),(0,20),(8,26),(15,26),(27,21),(22,12),(10,0)]
-#polygone=[]
+import math
+
+n = 7
+polygone = [(0, 10), (0, 20), (8, 26), (15, 26), (27, 21), (22, 12), (10, 0)]
+
+
+# polygone=[]
 
 def figure():
-    n=int(input("Nombre de sommet de la figure : "))
-    for i in range(0,n):
-        x=int(input("x = "))
-        y=int(input("y = "))
-        polygone.append((x,y))
+    n = int(input("Nombre de sommet de la figure : "))
+    for i in range(0, n):
+        x = int(input("x = "))
+        y = int(input("y = "))
+        polygone.append((x, y))
     print(polygone)
+
+
+def longueur(i, j):
+    a = polygone[i]
+    b = polygone[j]
+    xa = a[0]
+    ya = a[1]
+    xb = b[0]
+    yb = b[1]
+    u = xb - xa
+    v = yb - ya
+    return math.sqrt((u * u) + (v * v))
+
 
 def nbtri(n):
     c = n - 2

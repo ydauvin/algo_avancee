76945d6ce60e719c679fd3031810d10e4b0447ae
lleguen <louisleg29@gmail.com>
2020-04-16 19:42:11 +0200

    il ne reste plus que validecorde(i,j) réécrite avec les conditions qui manquaient

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import math
 
+import math
 from polygone import figure
 
 n = 7
 # tabcorde= [(0,2),(0,3),(3,5)]
 tabcorde = []
+
+C=[(1,3,8),(2,4,6)]
+longueur = 0
 
 
 def main():
@@ -17,6 +20,57 @@ def test():
         for p in range(0, n):
             if validecorde(y, p):
                 print("i :", y, "| j :", p)
+
+
+def validecorde(i, j):
+
+    l = len(tabcorde)
+
+    #il n'y a pas de corde si se sont les mêmes sommets
+    if(i==j) :
+        return False
+
+    # permutation pour avoir j>i
+    if (i > j):
+        tmp = j
+        j = i
+        i = tmp
+
+    #si i et j sont des sommets adjacents, on retourne Faux
+    if(j==i+1) or (j+i == n-1) :
+        return False
+    #On parcoure le tableau des cordes déjà tracées
+    for k in (0,l) :
+        (a,b)=tabcorde[k]
+
+        #si a >b, on permute les valeurs de a et b.
+        if (a > b):
+            tmp = b
+            b = a
+            a = tmp
+
+        #On vérifie si la corde est déjà tracée. Si elle l'est déjà, on retourne Faux
+        if (((i,j)==(a,b))or((i,j)==(b,a))) :
+            return False
+
+        #on vérifie que la corde (i,j) ne coupe pas les cordes déjà tracées. SI oui, on retourne Faux
+        if((a<i<b<j)or (i<a<j<b)) :
+            return False
+
+    return True
+
+def triangulation(nbCordeTracee, i) :
+
+    if (validecorde2(C[i][0],C[i][1])):
+        longueur= longueur+C[2]
+        if
+
+
+
+
+
+
+
 
 def essais_successifs():
     k = 0

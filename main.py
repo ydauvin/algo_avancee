16ff186e1a722153acc3ad587478115386c04bb6
ydauvin <51073361+ydauvin@users.noreply.github.com>
2020-04-17 13:30:34 +0200

    gitignore & +

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import math
-from polygone import figure
+from polygone import figure, valideCorde
 
 n = 7
 # tabcorde= [(0,2),(0,3),(3,5)]
@@ -18,39 +18,13 @@ def test():
     # affiche tout les cordes qui peuvent etre tracées
     for y in range(0, int(n / 2) + 1):
         for p in range(0, n):
-            if validecorde(y, p):
+            if valideCorde(y, p):
                 print("i :", y, "| j :", p)
 
 def triangulation(nbCordeTracee, i) :
-
-    if (validecorde2(C[i][0],C[i][1])):
-        longueur= longueur+C[2]
-        if
-
-
-
-
-
-
-
-
-def essais_successifs():
-    k = 0
-    for i in range(0, n):
-        while validecorde(i, k) == False:
-            k += 1
-            if k == n - 1:
-                break
-        if validecorde(i, k):
-            if i < k:
-                tabcorde.append((i, k))
-            else:
-                tmp = k
-                k = i
-                i = tmp
-                tabcorde.append((i, k))
-        k = 0
-        print(tabcorde)
+    longueur = 0
+    if (valideCorde(C[i][0],C[i][1])):
+        longueur = longueur+C[2]
 
 
 # doit toujours etre a la fin
